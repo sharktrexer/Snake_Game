ca7cab375f0b3e75933cd2ded74d04bf66b8980e
sharktrexer <sharktrexer@gmail.com>
2024-09-01 23:05:49 -0700

    RAINBOW MODE!

--- Mr_Snake_Game.py
+++ Mr_Snake_Game.py
@@ -16,8 +16,7 @@ GAME_NAME = 'Mr.Snake\'s Insatiable Appetite'
 fps = 30
 
 font = pygame.font.SysFont('subscribe',40)
-
-MAX_FRUIT = 10
+sub_font= pygame.font.SysFont('subscribe',30)
 
 #Colors
 BLACK = (0,0,0)
@@ -32,7 +31,11 @@ VIOLET = (128, 0, 255)
 RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
 
+# rainbow related vars
 RAINBOW = [RED, ORANGE, YELLOW, GREEN, CYAN, BLUE, VIOLET]
+DISABLE_RAINBOW_MODE = pygame.event.custom_type() # custom event that serves to disable rainbow mode
+RAINBOW_TIME_DURATION = 5000
+RAINBOW_SCORE_MULT = 2
 
 #game window initialize
 WIN = pygame.display.set_mode((WIN_X,WIN_Y))
@@ -40,7 +43,9 @@ WIN = pygame.display.set_mode((WIN_X,WIN_Y))
 #Game caption
 pygame.display.set_caption(GAME_NAME)
 
+# Fruit Vars
 UNIQUE_FRUIT_TYPES = 6
+MAX_FRUIT = 15
 
 class Fruit():
     def __init__(self, pos, fType):
@@ -78,7 +83,7 @@ class Fruit():
             
             pygame.draw.rect(WIN , RAINBOW[math.floor(self.rainIterator)], (self.pos[0],self.pos[1],10,10))
             
-            self.rainIterator += 0.26
+            self.rainIterator += 0.2
             if math.floor(self.rainIterator) >= len(RAINBOW):
                 self.rainIterator = 0
                 
@@ -94,11 +99,11 @@ def main_menu():
                 pygame.quit()
                 sys.exit()
             #Click to game
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.KEYDOWN:
                 main()
         
-        WIN.fill((0,0,0)) #BG
-        main_menu_message = font.render('Press anywhere to start the game' , True , WHITE)
+        WIN.fill(BLACK) #BG
+        main_menu_message = font.render('Press anything to start the game!' , True , WHITE)
         font_pos = main_menu_message.get_rect(center=(WIN_X//2, WIN_Y//2))
         WIN.blit(main_menu_message , font_pos)
         pygame.display.update()
@@ -110,27 +115,25 @@ def game_over(score):
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-        
-        WIN.fill((0,0,0)) #BG
-        
-        #showing 'You lost' in red color
-        game_over_message = font.render('You Lost' , True , RED)
-        #showing 'You score was SCORE'
-        game_over_score = font.render(f'Your Score was {score}' , True , WHITE)
-
-        font_pos_message = game_over_message.get_rect(center=(WIN_X//2, WIN_Y//2))
-        font_pos_score = game_over_score.get_rect(center=(WIN_X//2, WIN_Y//2+40))
-        WIN.blit(game_over_message , font_pos_message)
-        WIN.blit(game_over_score , font_pos_score)
-        pygame.display.update()
-
-        #Click to continue
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 main_menu()
+        
+        WIN.fill(BLACK) #BG
+        
+        #showing 'You lost' in red color
+        game_over_message = font.render('GAME OVER' , True , RED)
+        game_over_score = sub_font.render(f'Your Score was {score}' , True , WHITE)
+        continue_message = sub_font.render('Press anything to try again!' , True , CYAN)
+
+        font_pos_message = game_over_message.get_rect(center=(WIN_X//2, WIN_Y//2))
+        font_pos_score = game_over_score.get_rect(center=(WIN_X//2, WIN_Y//2-50))
+        font_pos_continue = continue_message.get_rect(center=(WIN_X//2, WIN_Y//2+50))
+        WIN.blit(game_over_message , font_pos_message)
+        WIN.blit(game_over_score , font_pos_score)
+        WIN.blit(continue_message, font_pos_continue)
+        pygame.display.update()
+
+
         
         
 #MAIN
@@ -168,6 +171,9 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
+                
+            if event.type == DISABLE_RAINBOW_MODE:
+                rainbow_mode = False
         
         #Control input
         keys = pygame.key.get_pressed()
@@ -179,24 +185,51 @@ def main():
         if (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and direction != 'left':
                 direction = 'right'
         if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and direction != 'right':
-                direction = 'left'        
+                direction = 'left'   
+                
+        #Rainbow affecting vars
+        bg_col = BLACK
+        score_mult = 1
+        sc_col = WHITE
+        rainbow_speed = 0
+        #Rainbow Mode Effects
+        if rainbow_mode:
+            bg_col = WHITE
+            sc_col = BLACK
+            score_mult = RAINBOW_SCORE_MULT 
+            rainbow_speed = 5
         
-        WIN.fill(BLACK) #background
+        WIN.fill(bg_col) #background
         
-        #Drawing Snake
-        for square in snake_body:
-            pygame.draw.rect(WIN ,GREEN, (square[0],square[1],10,10))
+        #Drawing Snake...
+        #As rainbow colors
+        if rainbow_mode:
+            ind = len(snake_body) - 1
+            rInd = 0
+            while(ind >= 0):
+                
+                pygame.draw.rect(WIN , RAINBOW[rInd], (snake_body[ind][0], snake_body[ind][1],10,10))
+                
+                ind -= 1
+                rInd += 1
+                if rInd == len(RAINBOW):
+                    rInd = 0
+        #As standard green
+        else:
+            for square in snake_body:
+                pygame.draw.rect(WIN ,GREEN, (square[0],square[1],10,10))
         
         
         #Directions
+        speed = 10 * speed_multiplier + rainbow_speed
         if direction == 'right':
-            snake_pos[0] += 10 * speed_multiplier
+            snake_pos[0] += speed
         elif direction == 'left':
-            snake_pos[0] -= 10 * speed_multiplier
+            snake_pos[0] -= speed
         elif direction == 'up':
-            snake_pos[1] -= 10 * speed_multiplier
+            snake_pos[1] -= speed
         elif direction == 'down':
-            snake_pos[1] += 10 * speed_multiplier
+            snake_pos[1] += speed
         
         #Body adding
         snake_body.append(list(snake_pos))
@@ -206,13 +239,13 @@ def main():
         while i < fruit_limit and len(fruits) < fruit_limit:
             doChooseRandNum = True
             while doChooseRandNum:
-                randNum = random.randint(5,UNIQUE_FRUIT_TYPES)
+                randNum = random.randint(-1,UNIQUE_FRUIT_TYPES)
                 doChooseRandNum = False
                 
                 #Stopping snake from being able to shrink below 3 squares
                 snakeTooSmall = len(snake_body) <= 3 and randNum == 4
                 #Stopping multiple rainbow fruit spawning or spawning when in rainbow mode
-                tooMuchRainbow = (rainbow_mode or aRainbowFruitExists(fruits)) and randNum == 6
+                tooMuchRainbow = (rainbow_mode or aRainbowFruitExists(fruits)) and randNum == 6 and fruit_limit > 2
                 
                 if snakeTooSmall or tooMuchRainbow: 
                     doChooseRandNum = True
@@ -224,15 +257,17 @@ def main():
         
         #Fruit draw AND effect
         fruit_ate = False
+        
+            
         for f in fruits:
             f.draw()
             if pygame.Rect(snake_pos[0],snake_pos[1],10,10).colliderect(pygame.Rect(f.pos[0],f.pos[1],10,10)):
                 #print("before: ", len(snake_body))
-                score += 5
+                score += 5 * score_mult
                 fruits.remove(f)
                 fruit_ate = True
                 if f.fType == 1: #Double Score                  
-                    score += 5
+                    score += 5 * score_mult
                 elif f.fType == 2: #Speed Up
                     speed_multiplier += 0.05
                 elif f.fType == 3: #Speed Down
@@ -243,27 +278,26 @@ def main():
                 elif f.fType == 5: #Grow Extra   
                     snake_body.append(list(snake_pos))
                 elif f.fType == 6: #RAINBOW 
-                    #rainbow_mode = True
-                    # temp functionality
-                    score += 50
-                #print("after: ", len(snake_body))
+                    rainbow_mode = True
+                    # Turns bool to true for set amount of time
+                    pygame.time.set_timer(pygame.event.Event(DISABLE_RAINBOW_MODE), RAINBOW_TIME_DURATION, 1)
+                    
             
         if fruit_ate == False:
             snake_body.pop(0) #Body shrink for movement        
         
         #Increasing fruit limit based on score
         if fruit_limit < MAX_FRUIT:
-            fruit_limit = int(score / 50) + 10
+            fruit_limit = int(score / 50) + 1
         
         #Score rendering
-        score_font = font.render(f'{score}' , True , WHITE)
+        score_font = font.render(f'{score}' , True , sc_col)
         font_pos = score_font.get_rect(center=(WIN_X//2-40 , 30))
         WIN.blit(score_font , font_pos)
         
         #Update
         pygame.display.update()
         clock.tick(fps)
-        print(pygame.time)
         
         #Body Hit, ignoring starting body elements (Game Over)
         for square in snake_body[:-3]: 
